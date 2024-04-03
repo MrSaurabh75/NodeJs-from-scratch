d9708987686851b19cd5c295294f434aea4ee5f1
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-04-03 21:11:48 +0530

    PATH Module

---
+++ path.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const a =path.basename('C:\\Users\\Saurabh\\Desktop\\Git\\NodeJs-from-scratch');
+console.log(a);
+const b =path.dirname('C:\\Users\\Saurabh\\Desktop\\Git\\NodeJs-from-scratch');
+console.log(b);
+const c =path.extname(__filename);
+console.log(c);
