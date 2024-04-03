f9d7681bc69136dae4a9fdfd5be47105b04b3c7e
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-04-03 21:32:40 +0530

    FS Module

---
+++ fs.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+// fs.readFile('prompt.txt','utf-8',(err,data)=>{
+//     console.log(err, data)
+// });
+// console.log('Finished');
+// const a =fs.readFileSync('prompt.txt');
+// console.log(a,"\n",a.toString());
+// console.log('Finished');
+
+// fs.writeFile("prompt.txt",'Saurabh Digambar Chorge',()=>{
+//     console.log("Written to the file")
+// });
+// console.log("Finished");
+// const a = fs.writeFileSync("prompt.txt","Raj Digambar Chorge");
+// console.log(a)
+// console.log("Finished");
+
